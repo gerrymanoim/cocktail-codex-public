d60dd5434ba9ea7f89eeaf3984231489c0258bee
gerrymanoim <gerrymanoim@gmail.com>
2022-03-31 01:34:16 -0400

    Delete recipes.sql

--- schemas/recipes.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE recipes (
-    name TEXT NOT NULL,
-    ingredients TEXT NOT NULL,
-    instructions TEXT NOT NULL,
-    notes TEXT,
-    FOREIGN KEY(post_id) REFERENCES posts(rowid)
-)
-;
